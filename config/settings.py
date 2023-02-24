575665b8c1c2b66b3b9fea39a2c917dae8323be1
khs <hyeongs2323@gmail.com>
2023-02-24 13:30:16 +0900

    feat: cors header setting

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     ########
     "rest_framework",
+    "corsheaders",
     ########
     "users.apps.UsersConfig",
     "feeds.apps.FeedsConfig",
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -129,3 +131,12 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
+
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000"
+]
+CORS_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000"
+]
